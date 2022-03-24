2ca72ed7fb65d16623715ba041470ba77756e06a
Efren Bernal <efrenbernal90@gmail.com>
2022-03-24 14:34:37 -0700

    update flask app and fixed funky thing with the model, added additional ML models

---
+++ Resources/ml.py
@@ -0,0 +1,11 @@
+import pickle
+
+
+# saves a Python object to a pickled file using a relative path from the ../data/ directory.
+def pickle_file(data_path, python_obj):
+    with open('./pickle_data/' + data_path, 'wb') as pickle_file:
+        pickle.dump(python_obj, pickle_file, protocol=pickle.DEFAULT_PROTOCOL)
+
+def load_file(data_path):
+    with open('./pickle_data/' + data_path, "rb") as input_file:
+        return pickle.load(input_file)
