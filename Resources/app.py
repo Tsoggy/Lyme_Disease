705494b8ee09080a9dd4e4f60b5bb9b7e86ab181
Efren Bernal <efrenbernal90@gmail.com>
2022-03-28 13:33:07 -0700

    updated ML models, include more webapp templates

--- Resources/app.py
+++ Resources/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, redirect, url_for, render_template, request, jsonify
 import pandas as pd
-import numpy as np
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 from ml import load_file
+
+# Initiate Flask App
+app = Flask(__name__)
 
 # Database setup
 engine = create_engine("sqlite:///lyme.sqlite")
@@ -13,22 +15,14 @@ engine = create_engine("sqlite:///lyme.sqlite")
 Base = automap_base()
 Base.prepare(engine, reflect=True)
 
-# Save reference to Demographics/Lyme Cases Table
+# # Save reference to Demographics/Lyme Cases Table
 LymeTable = Base.classes.ML_Demographic_LymeCase_HealthRank
-
-# Initiate Flask App
-app = Flask(__name__)
 
 # Import ML Models
 brfmodel = load_file('brfmodel.pkl')
 
 df = pd.DataFrame([{
-    'Lifespan_Rank': 76,
-    'Life_Quality_Rank': 83,
-    'Health_Behaviors_Rank': 71,
-    'Clinical_Care_Rank': 92,
-    'Social_Economic_Factors_Rank': 71,
-    'Physical_Environment_Rank': 80,
+    'Average_Health_Rank': 15.3,
     'Population': 7845.0,
     '%<18_Yrs_Old': 24.04,
     '%65_Yrs_Old_and_over': 19.6,
@@ -45,8 +39,7 @@ df = pd.DataFrame([{
     'Deaths(Count)': 116.0,
     'Lat': 41.0298,
     'Long': -92.869,
-    'Ticks_With_Lyme': 0,
-    'Norm_Incidence': 0.0}
+    'Ticks_With_Lyme': 0}
 ])
 
 
@@ -56,55 +49,86 @@ def home():
     print('++++++++++')
     print(prediction)
     print('++++++++++')
-    return render_template("index.html")
+    return render_template("fips.html")
 
 
-@app.route('/predict', methods=['POST', 'GET'])
-def predictions():
-    if request.method == "POST":
-        data = request.form.get("county", "state")
-        # float_features = [str(x) for x in data]
-        # features = [np.array(float_features)]
-        prediction = brfmodel.predict(df)
-        # print('++++++++++')
-        print(prediction)
-        # print('++++++++++')
-        try:
-            session = Session(engine)
+@app.route('/predict/<fips>', methods=['POST', 'GET'])
+# def predictions(fips):
+# if request.method == "POST":
+#     data = request.form.get("county", "state")
+#     # float_features = [str(x) for x in data]
+#     # features = [np.array(float_features)]
+#     prediction = brfmodel.predict(df)
+#     # print('++++++++++')
+#     # print(prediction)
+#     # print('++++++++++')
+#     try:
+#         session = Session(engine)
+#         results = session.query(LymeTable.County, LymeTable.State).filter(
+#             LymeTable.FIPS == data)
+#         parse_results = jsonify(pd.DataFrame(results).to_dict('records'))
+#         session.close()
+#         return redirect(url_for("index.html", prediction_text="The incidence of Lyme is {}".format(parse_results)))
+#     except:
+#         # data = pd.DataFrame([request.form])
+#         # print(data)
+#         return render_template('index.html', 404)
+# @app.route('/find/county/<fips>', methods='POST')
+# def find_county(fips):
+#     if request.method == 'POST':
+#         fips = request.form['state']
+#         return redirect(url_for('/'))
+@app.route("/makepred", methods=['POST', 'GET'])
+def makepred():
+    form = request.form['st']
+    # if form
+    session = Session(engine)
 
-            results = session.query(LymeTable.County, LymeTable.State).filter(
-                LymeTable.FIPS == data)
-            parse_results = pd.DataFrame(results).to_dict('records')
-            session.close()
-            return redirect(url_for("index.html", prediction_text="The incidence of Lyme is {}".format(parse_results)))
-        except:
-            # data = pd.DataFrame([request.form])
-            # print(data)
+    results = session.query(LymeTable.FIPS, LymeTable.County,
+                            LymeTable.State, LymeTable.Population,
+                            LymeTable.Ticks_With_Lyme, LymeTable.Norm_Incidence).all()
 
-            return render_template('index.html', 404)
+    session.close
+    county_summary = []
+    for fips, cases, county, state, pop, ticks in results:
+        counties_dict = {}
+        counties_dict['FIPS'] = fips
+        counties_dict['County'] = county
+        counties_dict['State'] = state
+        # counties_dict['Average_Health_Rank'] = avg
+        counties_dict['Population'] = pop
+        counties_dict['Ticks_With_Lyme'] = ticks
+        counties_dict['Incidences of Lyme Reported per 1000'] = cases
+
+        county_summary.append(counties_dict)
+        datas = jsonify(county_summary)
+    return render_template("hello Efren", datas=datas)
 
 
-@app.route('/counties')
+@app.route('/counties', methods=['POST', 'GET'])
 def counties():
+    # cur =
     session = Session(engine)
 
     results = session.query(LymeTable.Norm_Incidence,
                             LymeTable.County, LymeTable.State, LymeTable.FIPS).all()
     session.close()
     county_summary = []
-    for cases, county, state, fips in results:
+    for fips, cases, county, state in results:
         counties_dict = {}
-        counties_dict['Incidences of Lyme Reported per 1000'] = cases
+        counties_dict['FIPS'] = fips
         counties_dict['County'] = county
         counties_dict['State'] = state
-        counties_dict['FIPS'] = fips
+        counties_dict['Incidences of Lyme Reported per 1000'] = cases
+
         county_summary.append(counties_dict)
 
     return jsonify(county_summary)
 
-# @app.route('/ML_Model', methods=['POST','GET'])
-# def ml_model():
-#     return render_template()
+
+@app.route('/state/info', methods=['POST', 'GET'])
+def state_info():
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
